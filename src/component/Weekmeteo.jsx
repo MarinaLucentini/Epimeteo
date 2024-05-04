7a7268d7536b18b7a47b7720bfd3b7573b051cdc
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-05-04 20:48:44 +0200

    filter

--- src/component/Weekmeteo.jsx
+++ src/component/Weekmeteo.jsx
@@ -52,174 +52,29 @@ const Weekmeteo = (props) => {
       {meteo && (
         <Container>
           <Row xs={6} className="flex-row">
-            <Col>
-              {meteo && (
-                <Card>
-                  <Card.Body>
-                    <Card.Title className="d-flex flex-column align-items-center">
-                      <p>
-                        {getDayOfTheWeek(
-                          meteo.list[5].dt_txt
-                        )}
-                      </p>
-                      <h3>
-                        {Math.round(
-                          meteo.list[5].main.temp
-                        )}{" "}
-                        °
-                      </h3>
-                      <h4 className="mx-3 d-flex align-items-center">
-                        {meteo.list[5].weather[0].main}
-                        <img
-                          src={`http://openweathermap.org/img/wn/${meteo.list[5].weather[0].icon}.png`}
-                          alt=""
-                        />
-                      </h4>
-                    </Card.Title>
-                  </Card.Body>
-                </Card>
-              )}
-            </Col>
-            <Col>
-              {meteo && (
-                <Card>
-                  <Card.Body>
-                    <Card.Title className="d-flex flex-column align-items-center">
-                      <p>
-                        {getDayOfTheWeek(
-                          meteo.list[10].dt_txt
-                        )}
-                      </p>
-                      <h3>
-                        {Math.round(
-                          meteo.list[10].main.temp
-                        )}{" "}
-                        °
-                      </h3>
-                      <h4 className="mx-3 d-flex align-items-center">
-                        {meteo.list[10].weather[0].main}
-                        <img
-                          src={`http://openweathermap.org/img/wn/${meteo.list[10].weather[0].icon}.png`}
-                          alt=""
-                        />
-                      </h4>
-                    </Card.Title>
-                  </Card.Body>
-                </Card>
-              )}
-            </Col>
-            <Col>
-              {meteo && (
-                <Card>
-                  <Card.Body>
-                    <Card.Title className="d-flex flex-column align-items-center">
-                      <p>
-                        {getDayOfTheWeek(
-                          meteo.list[15].dt_txt
-                        )}
-                      </p>
-                      <h3>
-                        {Math.round(
-                          meteo.list[15].main.temp
-                        )}{" "}
-                        °
-                      </h3>
-                      <h4 className="mx-3 d-flex align-items-center">
-                        {meteo.list[15].weather[0].main}
-                        <img
-                          src={`http://openweathermap.org/img/wn/${meteo.list[15].weather[0].icon}.png`}
-                          alt=""
-                        />
-                      </h4>
-                    </Card.Title>
-                  </Card.Body>
-                </Card>
-              )}
-            </Col>
-            <Col>
-              {meteo && (
-                <Card>
-                  <Card.Body>
-                    <Card.Title className="d-flex flex-column align-items-center">
-                      <p>
-                        {getDayOfTheWeek(
-                          meteo.list[20].dt_txt
-                        )}
-                      </p>
-                      <h3>
-                        {Math.round(
-                          meteo.list[20].main.temp
-                        )}{" "}
-                        °
-                      </h3>
-                      <h4 className="mx-3 d-flex align-items-center">
-                        {meteo.list[20].weather[0].main}
-                        <img
-                          src={`http://openweathermap.org/img/wn/${meteo.list[20].weather[0].icon}.png`}
-                          alt=""
-                        />
-                      </h4>
-                    </Card.Title>
-                  </Card.Body>
-                </Card>
-              )}
-            </Col>
-            <Col>
-              {meteo && (
-                <Card>
-                  <Card.Body>
-                    <Card.Title className="d-flex flex-column align-items-center">
-                      <p>
-                        {getDayOfTheWeek(
-                          meteo.list[25].dt_txt
-                        )}
-                      </p>
-                      <h3>
-                        {Math.round(
-                          meteo.list[25].main.temp
-                        )}{" "}
-                        °
-                      </h3>
-                      <h4 className="mx-3 d-flex align-items-center">
-                        {meteo.list[25].weather[0].main}
-                        <img
-                          src={`http://openweathermap.org/img/wn/${meteo.list[25].weather[0].icon}.png`}
-                          alt=""
-                        />
-                      </h4>
-                    </Card.Title>
-                  </Card.Body>
-                </Card>
-              )}
-            </Col>
-            <Col>
-              {meteo && (
-                <Card>
-                  <Card.Body>
-                    <Card.Title className="d-flex flex-column align-items-center">
-                      <p>
-                        {getDayOfTheWeek(
-                          meteo.list[30].dt_txt
-                        )}
-                      </p>
-                      <h3>
-                        {Math.round(
-                          meteo.list[30].main.temp
-                        )}{" "}
-                        °
-                      </h3>
-                      <h4 className="mx-3 d-flex align-items-center">
-                        {meteo.list[30].weather[0].main}
-                        <img
-                          src={`http://openweathermap.org/img/wn/${meteo.list[30].weather[0].icon}.png`}
-                          alt=""
-                        />
-                      </h4>
-                    </Card.Title>
-                  </Card.Body>
-                </Card>
-              )}
-            </Col>
+            {meteo.list
+              .filter((item, index) => index % 8 === 0)
+              .map((day, index) => (
+                <Col key={index}>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title className="d-flex flex-column align-items-center">
+                        <p>{getDayOfTheWeek(day.dt_txt)}</p>
+                        <h3>
+                          {Math.round(day.main.temp)} °
+                        </h3>
+                        <h4 className="mx-3 d-flex align-items-center">
+                          {day.weather[0].main}
+                          <img
+                            src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+                            alt=""
+                          />
+                        </h4>
+                      </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
           </Row>
         </Container>
       )}
